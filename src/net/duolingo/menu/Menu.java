74525c5e88f600160bf56fa0fcd532af06d635fc
JudelRos <usr3851@salesianos-lacuesta.net>
2024-05-07 13:22:29 +0100

    feat(Menu): added class Menu with its showing method

---
+++ src/net/duolingo/menu/Menu.java
@@ -0,0 +1,19 @@
+package net.duolingo.menu;
+
+public class Menu {
+    final String MENU = """
+            ****************DICCIONARIO DUOLINGO****************
+            --- MENU DE USUARIO ---
+                1.- Add word
+                2.- Delete word
+                3.- Does this word exist?
+                4.- Show valid initials
+                5.- Show words by initial
+                0.- Close program
+            -------------------------
+                """;
+
+    public void showMenu() {
+        System.out.println(MENU);
+    }
+}
